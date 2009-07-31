3e8ca7e856a1256ff37e985fee369e36da6f8fad
Alastair Robinson <amr@amr-desktop.(none)>
2009-07-31 01:26:54 +0100

    Added ColorantSelector to test.cpp

--- test.cpp
+++ test.cpp
@@ -1,39 +1,98 @@
 #include <iostream>
 
+#include <gtk/gtk.h>
+
 #include "support/progresstext.h"
 #include "imageutils/tiffsave.h"
 #include "imagesource/imagesource_util.h"
-
+#include "imagesource/pixbuf_from_imagesource.h"
+#include "miscwidgets/pixbufview.h"
+#include "miscwidgets/colorantselector.h"
 #include "cachedimage.h"
+
+#include "config.h"
+#include "gettext.h"
+#define _(x) gettext(x)
+
 
 using namespace std;
 
 
+class TestUI
+{
+	public:
+	TestUI() : window(NULL), colsel(NULL), pbview(NULL), image(NULL), collist(NULL)
+	{
+		window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		gtk_window_set_default_size(GTK_WINDOW(window),600,450);
+		gtk_window_set_title (GTK_WINDOW (window), _("TestUI"));
+		gtk_signal_connect (GTK_OBJECT (window), "delete_event",
+			(GtkSignalFunc) gtk_main_quit, NULL);
+		gtk_widget_show(window);
+
+		GtkWidget *hbox=gtk_hbox_new(FALSE,0);
+		gtk_container_add(GTK_CONTAINER(window),hbox);
+		gtk_widget_show(GTK_WIDGET(hbox));
+
+		colsel=colorantselector_new(NULL);
+		gtk_box_pack_start(GTK_BOX(hbox),colsel,FALSE,FALSE,0);
+		gtk_widget_show(colsel);
+
+		pbview=pixbufview_new(NULL,false);
+		gtk_box_pack_start(GTK_BOX(hbox),pbview,TRUE,TRUE,0);
+		gtk_widget_show(pbview);
+	}
+
+	~TestUI()
+	{
+		if(collist)
+			delete collist;
+		if(image)
+			delete image;
+	}
+
+	void SetImage(const char *filename)
+	{
+		ImageSource *is=ISLoadImage(filename);
+
+		if(collist)
+			delete collist;
+		collist=new DeviceNColorantList(is->type);
+		colorantselector_set_colorants(COLORANTSELECTOR(colsel),collist);
+
+		image=new CachedImage(is);
+		Redraw();
+	}
+
+	void Redraw()
+	{
+		ImageSource *is=image->GetImageSource();
+		GdkPixbuf *pb=pixbuf_from_imagesource(is);
+		pixbufview_set_pixbuf(PIXBUFVIEW(pbview),pb);
+		g_object_unref(G_OBJECT(pb));
+		delete is;
+	}
+	protected:
+	GtkWidget *window;
+	GtkWidget *colsel;
+	GtkWidget *pbview;
+	CachedImage *image;
+	DeviceNColorantList *collist;
+};
+
+
 int main(int argc,char **argv)
 {
+	gtk_init(&argc,&argv);
 	try
 	{
-		if(argc==3)
-		{
-			ImageSource *is=ISLoadImage(argv[1]);
-			CachedImage cache(is);
+		TestUI ui;
 
-			is=cache.GetImageSource();
-			ProgressText prog;
-			TIFFSaver t(argv[2],is);
-			t.SetProgress(&prog);
-			t.Save();
-			delete is;
+		if(argc>1)
+			ui.SetImage(argv[1]);
 
-			is=cache.GetImageSource();
-			TIFFSaver t2(argv[2],is);
-			t2.SetProgress(&prog);
-			t2.Save();
-			delete is;
+		gtk_main();
 
-		}
-		else
-			cerr << "Usage: " << argv[0] << " in.tif out.tif" << endl;
 	}
 	catch(const char *err)
 	{
